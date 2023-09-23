b597b65c35c85d6db788f6841c8b3480ad9c9f19
zinh9 <127460831+zinh9@users.noreply.github.com>
2023-09-23 12:35:39 -0300

    Create Exe 10.py

---
+++ Exe 10.py
@@ -0,0 +1,6 @@
+palavras = ("caneta", "azul", "azul", "caneta", "caneta", "que", "esta", "marcada", "com", "a", "minha", "letra")
+
+for p in palavras:
+    print(f"Palavra: {p}")
+    print(f"Tamanho: {len(p)}")
+    print("-----------------------------------------------------------")
