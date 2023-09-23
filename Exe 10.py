22e185740706f964709545464e298815b870531b
zinh9 <127460831+zinh9@users.noreply.github.com>
2023-09-23 12:39:37 -0300

    Update Exe 10.py

--- Exe 10.py
+++ Exe 10.py
@@ -1,6 +1,10 @@
 palavras = ("caneta", "azul", "azul", "caneta", "caneta", "que", "esta", "marcada", "com", "a", "minha", "letra")
+comprimento = []
 
 for p in palavras:
-    print(f"Palavra: {p}")
-    print(f"Tamanho: {len(p)}")
-    print("-----------------------------------------------------------")
+    comprimento.append(len(p))
+
+comprimento = tuple(comprimento)
+
+print(f"Palavras: {palavras}")
+print(f"Comprimento de cada palavra: {comprimento}")
